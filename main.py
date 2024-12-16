de7c1949b8ff87a33aa3ca16a98d4bbe32df76cb
JOSEMML07 <josemiguelmanrique071207@gmail.com>
2024-12-16 18:20:29 +0100

    modified:   main.py 	new file:   templates/feedback.html 	modified:   templates/index.html

--- main.py
+++ main.py
@@ -18,16 +18,13 @@ def process_form():
     button_discord = request.form.get('button_discord')
     button_html = request.form.get('button_html')
     button_db = request.form.get('button_db')
-
-    
     return render_template('index.html', button_python=button_python, button_discord=button_discord, button_html=button_html, button_db=button_db)
 
 @app.route('/feedback', methods=['POST'])
-def process_form():
+def process_feedback():
     email = request.form['email']
     text = request.form['text']
-
-    return render_template('feedback.html', email = email, text = text)
+    return render_template('feedback.html', email=email, text=text)
 
 if __name__ == "__main__":
     app.run(debug=True)
